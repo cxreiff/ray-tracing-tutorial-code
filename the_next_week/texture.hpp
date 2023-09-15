049adba7ba0f956d39876d11937bce9eae2fa55b
cxreiff <cooper@cxreiff.com>
2023-09-15 12:50:08 -0700

    marble-like texture by perlin turbulence

--- the_next_week/texture.hpp
+++ the_next_week/texture.hpp
@@ -84,7 +84,8 @@ public:
   noise_texture(double _scale) : scale(_scale) {}
 
   color value(double u, double v, const point3& p) const override {
-    return color(1, 1, 1) * noise.noise(p * scale);
+    auto s = scale * p;
+    return color(1, 1, 1) * 0.5 * (1 + sin(s.z() + 10 * noise.turb(s)));
   }
 
 private:
