02052cf559da945114c2acae320c845fd86cd63a
cxreiff <cooper@cxreiff.com>
2023-10-18 22:02:20 -0700

    volumes, issue with final_scene

--- the_next_week/interval.hpp
+++ the_next_week/interval.hpp
@@ -36,15 +36,15 @@ class interval {
     static const interval empty, universe;
 };
 
-const static interval empty(+infinity, -infinity);
-const static interval universe(-infinity, +infinity);
+const interval interval::empty = interval(+infinity, -infinity);
+const interval interval::universe = interval(-infinity, +infinity);
 
-interval operator+(const interval& ival, double displacement) {
-  return interval(ival.min + displacement, ival.max + displacement);
+interval operator+(const interval &ival, double displacement) {
+    return interval(ival.min + displacement, ival.max + displacement);
 }
 
-interval operator+(double displacement, const interval& ival) {
-  return ival + displacement;
+interval operator+(double displacement, const interval &ival) {
+    return ival + displacement;
 }
 
 #endif
